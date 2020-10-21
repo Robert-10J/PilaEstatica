df9765d51eb5e1b6316851a2db501b7a65e70df3
Roberto Torres <roberttv17@gmail.com>
2020-10-21 17:32:56 -0500

    Metodo insertar y estado mejorados

--- src/com/Pila/Pila.java
+++ src/com/Pila/Pila.java
@@ -1,24 +1,27 @@
 package com.Pila;
 import javax.swing.*;
-import java.awt.*;
 import java.util.Stack;
 public class Pila {
 
     Stack pil = new Stack();
     String pila [] = new String[5];
-    int element = 0;
-
-    public boolean vacio(){
-        return element <= 0;
-    }
 
     public void insertar() {
-        for (int i = 0; i < pila.length; i++){
-            String dato = JOptionPane.showInputDialog(null,
-                    "Ingrese al valor");
-            pila[i] = dato;
-            pil.push(pila[i]);
-        }
+
+            if (pila.length == pil.size()){
+                JOptionPane.showMessageDialog(null,"La pila esta llena",
+                        "No puede ingresar mas datos", JOptionPane.WARNING_MESSAGE);
+            } else if (pila == null ||pil.isEmpty()){
+                for (int i = 0; i < pila.length; i++){
+                    String dato = JOptionPane.showInputDialog(null,
+                            "Ingrese al valor");
+                    pila[i] = dato;
+                    pil.push(pila[i]); //Transformamos el arreglo en una lista
+                }
+            } else {
+                JOptionPane.showMessageDialog(null,"Pila no esta lista para recibir mas datos",
+                        "No puede introducir mas datos", JOptionPane.WARNING_MESSAGE);
+            }
     }
     public Object delete(){return pil.remove(pil.size() - 1);}
 
@@ -49,8 +52,7 @@ public class Pila {
             JOptionPane.showMessageDialog(null,
                     "Por favor elija la opcion 1", "Mo hay datos", JOptionPane.WARNING_MESSAGE);
         } else {
-            //JOptionPane.showMessageDialog(null, "ultimo dato" + pila(inElement - 1));
-            JOptionPane.showMessageDialog(null, "Ultimo dato" +  pil.get(pil.size() - 1));
+            JOptionPane.showMessageDialog(null, "Ultimo dato " +  pil.get(pil.size() - 1));
         }
     }
 
@@ -59,7 +61,7 @@ public class Pila {
             JOptionPane.showMessageDialog(null,
                     "Por favor elija la opcion 1", "Mo hay datos", JOptionPane.WARNING_MESSAGE);
         }  else {
-            JOptionPane.showMessageDialog(null,"Pila llena ");
+            JOptionPane.showMessageDialog(null,"La pila posee  " + pil.size() + " elementos");
         }
     }
 }
